53f6c132ad266c7bb806c84957b2b626bd265892
Mahmut Efe Cün <mecgithub@gmail.com>
2025-12-05 18:10:36 +0300

    feat(calculate): Ürün hesaplamaları güncellendi. Hesaplamalar KDV ye uygun yapılıyor.

--- public/assets/js/calculate.js
+++ public/assets/js/calculate.js
@@ -14,24 +14,18 @@ function calculate() {
   const birim_kar = document.getElementById("birim_kar");
   const envanter_deger = document.getElementById("envanter_deger");
 
-  // Değer yoksa dur
   if (isNaN(alis) || isNaN(satis) || isNaN(kdv) || isNaN(mevcut_stok)) return;
 
-  // Hesaplamalar
-  const satis_kdvli = satis + (satis * kdv) / 100;
-  const kar = satis_kdvli - alis;
+  const kdv_maliyeti = (satis * kdv) / 100;
+  const kar = satis - alis - kdv_maliyeti;
   const marj = ((kar / alis) * 100).toFixed(2);
 
-  // DOM'a yaz
   kar_marji.textContent = `%${marj}`;
-  birim_kar.textContent = `${satis - alis}₺`;
+  birim_kar.textContent = `${(satis - alis).toFixed(2)}₺`;
   envanter_deger.textContent = `${(mevcut_stok * alis).toLocaleString()}₺`;
-
-  console.log("Kar Marjı:", marj);
 }
 
 const calcInps = document.querySelectorAll(".calc-inp");
-
 calcInps.forEach((input) => {
   input.addEventListener("input", () => {
     calculate();
